38e7e8d243af54a462ee70f2b05f8c948f51958b
maxamed1st <maxamed1st@icloud.com>
2023-05-06 00:14:18 +0100

    create logic to navigate the tabs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import "./style.css";
-import skeleton from "./skeleton";
-let content = document.getElementById('content');
-content.appendChild(skeleton());
+import home from "./pages/home";
+import menu from "./pages/menu";
+import about from "./pages/about";
+import { navbar, footer } from "./skeleton";
+import { clearNode } from "./DOM";
+let container = document.getElementById('container');
+function handleClick(e) {
+    clearNode(container);
+    let target = e.target.id.toLowerCase();
+    switch (target) {
+        case 'home':
+            main(home);
+            break;
+        case 'menu':
+            main(menu);
+            break;
+        case 'about':
+            main(about);
+            break;
+        default:
+            main(home);
+            break;
+    };
+}
+function main(content) {
+    let nav = navbar(handleClick);
+    content = content();
+    let foot = footer();
+    container.appendChild(nav);
+    container.appendChild(content);
+    container.appendChild(foot);
+}
+main(home)

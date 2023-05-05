8133ce34771721942692682dee4385494d7127f4
maxamed1st <maxamed1st@icloud.com>
2023-05-06 00:10:54 +0100

    Add onclick handler to navItems

--- src/skeleton.js
+++ src/skeleton.js
@@ -1,34 +1,27 @@
-const newElement = function(type, text=null) {
-    //create new element of type and return the element
-    const element = document.createElement(type);
-    //if a text is passed add it to the element
-    if(text !== null) {
-        element.textContent = text;
-    };
-    return element;
-};
-const navbar = function() {
+import { newElement } from "./DOM";
+export function navbar(handleClick) {
     //create navbar and a list of the tabs
-    let nav = newElement("nav");
-    let ul = newElement("ul");
-    const liText = ["Home", "Menu", "Text"];
+    let nav = newElement("nav", "nav");
+    let ul = newElement("ul", "navlist");
+    const liText = ["Home", "Menu", "About"];
 
     //Loop through liText array to create the tabs
     for(let i=0; i<3; i++) {
-        let li = newElement("li", liText[i]);
+        let id = liText[i].toLowerCase();
+        let li = newElement("li", liText[i], id);
         li.classList.add("navItem");
+        li.addEventListener("click", handleClick);
         ul.appendChild(li);
     };
     
-    //Add id to the unordered list and append to the navigation bar
-    ul.setAttribute("id", "navList");
+    //Append the unordered list to the navigation bar
     nav.appendChild(ul);
 
     return nav;
 
 };
-const skeleton = function() {
-    //create a skeleton for a restaurant page that can be used on all pages
-    return navbar();
+
+export function footer() {
+    let foot = newElement("div", "Footer", "©2023 Restaurant");
+    return foot;
 }
-export default skeleton;
